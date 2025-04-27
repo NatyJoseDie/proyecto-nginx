caa0e2c0c719a025cb92382685c1db614d117635
NatyJoseDie <168145408+NatyJoseDie@users.noreply.github.com>
2025-04-27 16:28:04 -0300

    tiene ver encuesta, modificar y borrar, lo mismo para pregunta

--- src/modules/encuestas/encuestas.module.ts
+++ src/modules/encuestas/encuestas.module.ts
@@ -1,14 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { EncuestasController } from './controllers/encuestas.controller';
-import { EncuestasService } from './services/encuestas.service';
-import { Encuesta } from './entities/encuesta.entity';
+import { PreguntasController } from './controllers/preguntas.controller';
+import { PreguntasService } from './services/preguntas.service';
 import { Pregunta } from './entities/pregunta.entity';
 import { Opcion } from './entities/opcion.entity';
+import { Encuesta } from './entities/encuesta.entity';
+import { EncuestasController } from './controllers/encuestas.controller';
+import { EncuestasService } from './services/encuestas.service';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Encuesta, Pregunta, Opcion])],
-    controllers: [EncuestasController],
-    providers: [EncuestasService],
+  imports: [
+    TypeOrmModule.forFeature([Pregunta, Opcion, Encuesta])
+  ],
+  controllers: [PreguntasController, EncuestasController],
+  providers: [PreguntasService, EncuestasService],
+  exports: [PreguntasService, EncuestasService],
 })
 export class EncuestasModule {}
