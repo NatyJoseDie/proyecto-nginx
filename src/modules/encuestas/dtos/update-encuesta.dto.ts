12c4ebbdab517706b0eee8e9ec6a89a62cb1a54e
Leandro Andres Angeli <leandroandresangeli@gmail.com>
2025-05-16 21:41:17 -0300

    updating var values

--- src/modules/encuestas/dtos/update-encuesta.dto.ts
+++ src/modules/encuestas/dtos/update-encuesta.dto.ts
@@ -1,4 +1,4 @@
 import { PartialType } from '@nestjs/swagger';
-import { CreateEncuestaDto } from './create-encuesta.dto';
+import { CreateEncuestaDTO } from './create-encuesta.dto';
 
-export class UpdateEncuestaDto extends PartialType(CreateEncuestaDto) {}
+export class UpdateEncuestaDto extends PartialType(CreateEncuestaDTO) {}
--- src/modules/encuestas/dtos/update-pregunta.dto.ts
+++ src/modules/encuestas/dtos/update-pregunta.dto.ts
@@ -1,4 +1,4 @@
 import { PartialType } from '@nestjs/swagger';
-import { CreatePreguntaDto } from './create-pregunta.dto';
+import { CreatePreguntaDTO } from './create-pregunta.dto';
 
-export class UpdatePreguntaDto extends PartialType(CreatePreguntaDto) {}
+export class UpdatePreguntaDto extends PartialType(CreatePreguntaDTO) {}
