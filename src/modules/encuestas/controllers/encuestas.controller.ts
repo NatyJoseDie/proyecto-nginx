eea357c6a3d55177d42b032a6b53163b26b3beb9
Noelia <noeerivaas@gmail.com>
2025-05-29 17:39:37 -0300

    Conflicto solucionado

--- src/modules/encuestas/controllers/encuestas.controller.ts
+++ src/modules/encuestas/controllers/encuestas.controller.ts
@@ -1,24 +1,10 @@
-<<<<<<< HEAD
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Param,
-  Delete,
-  Patch,
-} from '@nestjs/common';
-=======
 import { Body, Controller, Get, Post, Param, Query } from '@nestjs/common';
 
->>>>>>> 9c179d70759e456d011e0f70babb6dfb28922e2a
 import { EncuestasService } from '../services/encuestas.service';
 import { CreateEncuestaDTO } from '../dtos/create-encuesta.dto';
 
 import { ObtenerEncuestaDTO } from '../dtos/obtener-encuesta.dto';
 import { Encuesta } from '../entities/encuesta.entity';
-import { ObtenerResultadosEncuestaDTO } from '../dtos/obtener-resultados-dto';
-import { ResultadosDto } from '../dtos/resultados.dto';
 
 @Controller('/encuestas')
 export class EncuestasController {
@@ -43,33 +29,5 @@ export class EncuestasController {
       dto.codigo,
       dto.tipo,
     );
-  }
-
-<<<<<<< HEAD
-  @Post()
-  create(@Body() createEncuestaDto: CreateEncuestaDto) {
-    return this.encuestasService.create(createEncuestaDto);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: number, @Body() body: any) {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-    return this.encuestasService.update(id, body);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: number) {
-    return this.encuestasService.remove(id);
-=======
-  @Get('/resultados/:id')
-  async obtenerResultadosEncuesta(
-    @Param('id') id: number,
-    @Query() dto: ObtenerResultadosEncuestaDTO,
-  ): Promise<ResultadosDto> {
-    return await this.encuestasService.obtenerResultadosEncuesta(
-      id,
-      dto.codigo,
-    );
->>>>>>> 9c179d70759e456d011e0f70babb6dfb28922e2a
   }
 }
